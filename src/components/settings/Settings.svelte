ef4bcf9cab5cf86ee11a7a955b446c0ce6f0b4a0
AppleJack9000 <101475399+AppleJack9000@users.noreply.github.com>
2025-05-15 21:51:51 +0300

    Updated manifest.json

--- src/components/settings/Settings.svelte
+++ src/components/settings/Settings.svelte
@@ -26,44 +26,41 @@
 	}
 </script>
 
-<!-- not currently supported, see https://github.com/sveltejs/svelte/issues/3105 -->
-<!-- <svelte:body class:light={!$settings.dark} class:colorblind={$settings.colorblind} /> -->
 <div class="outer">
 	<div class="settings-top">
-		<h3>settings</h3>
+		<h3>настройки</h3>
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<div
 			on:click={() => {
 				if (!state.validHard) {
-					toaster.pop("Game has already violated hard mode");
+					toaster.pop("Игра уже нарушила правила сложного режима");
 				}
 			}}
 		>
 			<Setting type="switch" bind:value={$settings.hard[$mode]} disabled={!state.validHard}>
-				<svelte:fragment slot="title">Hard Mode</svelte:fragment>
+				<svelte:fragment slot="title">Сложный режим</svelte:fragment>
 				<svelte:fragment slot="desc">
-					Any revealed hints must be used in subsequent guesses
+					Все открытые подсказки должны использоваться в последующих догадках
 				</svelte:fragment>
 			</Setting>
 		</div>
 		<Setting type="switch" bind:value={$settings.dark}>
-			<svelte:fragment slot="title">Dark Theme</svelte:fragment>
+			<svelte:fragment slot="title">Тёмная тема</svelte:fragment>
 		</Setting>
 		<Setting type="switch" bind:value={$settings.colorblind}>
-			<svelte:fragment slot="title">Color Blind Mode</svelte:fragment>
-			<svelte:fragment slot="desc">High contrast colors</svelte:fragment>
+			<svelte:fragment slot="title">Режим для дальтоников</svelte:fragment>
+			<svelte:fragment slot="desc">Высококонтрастные цвета</svelte:fragment>
 		</Setting>
 		<Setting type="dropdown" bind:value={$mode} options={modeData.modes.map((e) => e.name)}>
-			<svelte:fragment slot="title">Game Mode</svelte:fragment>
+			<svelte:fragment slot="title">Режим игры</svelte:fragment>
 			<svelte:fragment slot="desc">
-				The game mode determines how often the word refreshes
+				Режим игры определяет, как часто обновляется слово
 			</svelte:fragment>
 		</Setting>
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<Setting type="custom" bind:value={$mode}>
-			<svelte:fragment slot="title">Play Historical Game</svelte:fragment>
+			<svelte:fragment slot="title">Играть в историческую игру</svelte:fragment>
 			<svelte:fragment slot="desc">
-				Play a previous word by pasting in a link or setting the date number
+				Сыграйте в предыдущее слово, вставив ссылку или установив номер даты
 			</svelte:fragment>
 			<svelte:fragment slot="custom">
 				<svg
@@ -80,10 +77,10 @@
 		</Setting>
 		<div class="links">
 			<a href="https://github.com/MikhaD/wordle" target="_blank" rel="noreferrer">
-				Leave a ⭐
+				Поставить ⭐
 			</a>
 			<a href="https://github.com/MikhaD/wordle/issues" target="_blank" rel="noreferrer">
-				Report a Bug
+				Сообщить об ошибке
 			</a>
 		</div>
 	</div>
